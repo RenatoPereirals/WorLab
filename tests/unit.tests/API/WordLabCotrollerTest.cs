dbac30d6faddab6c56365c57d566b14e72888608
RenatoPereirals <renatopreirals@gmail.com>
2024-05-12 20:55:26 -0300

    test: Add placeholder unit test for WordLabController insertion method
    - Implement placeholder test to ensure test setup
    - Test currently returns true, to be implemented
    
    #1

---
+++ tests/unit.tests/API/WordLabCotrollerTest.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace test.unit.tests.API
+{
+    public class WordLabCotrollerTest
+    {
+        [Fact]
+        public void TestInsertWord_WhenWordInserted_ThenSuccess()
+        {
+            Assert.True(true);
+        }
+    }
+}
