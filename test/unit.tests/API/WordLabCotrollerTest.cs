c4530a218680633443932fbf166f049b16e1183d
RenatoPereirals <renatopreirals@gmail.com>
2024-05-15 20:56:42 -0300

    test: Update WordLabControllerTest to use Moq for mocking
    - Refactor test setup to utilize Moq for mocking dependencies
    - Add additional test case for handling exceptions with status code 400
    
    #3

--- test/unit.tests/API/WordLabCotrollerTest.cs
+++ test/unit.tests/API/WordLabCotrollerTest.cs
@@ -1,6 +1,7 @@
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.OpenApi.Any;
 using Moq;
+
+using Microsoft.AspNetCore.Mvc;
+
 using WordLab.API.Controllers;
 using WordLab.API.Interfaces;
 
@@ -8,20 +9,45 @@ namespace test.unit.tests.API
 {
     public class WordLabCotrollerTest
     {
+        private readonly Mock<IWordApplication> _mockWordApplication;
+
+        public WordLabCotrollerTest()
+        {
+            _mockWordApplication = new Mock<IWordApplication>();
+        }
+
         [Fact]
         public async void InsertWord_WhenWordInserted_ReturnsSuccessWithStatusCode201()
         {
             // Arrange
             var expectedWord = "test";
+            _mockWordApplication.Setup(res => res.AddWord(expectedWord)).ReturnsAsync(true);
 
             // Act
-            var wordController = new WordLabController();
+            var wordController = new WordLabController(_mockWordApplication.Object);
             var result = await wordController.InsertionWord(expectedWord);
 
             // Assert
             Assert.IsType<StatusCodeResult>(result);
             var statusCodeResult = (StatusCodeResult)result;
             Assert.Equal(201, statusCodeResult.StatusCode);
+        }
+
+        [Fact]
+        public async void InsertWord_WhenWordInserted_ReturnsExceptionWithStatusCode400()
+        {
+            // Arrange
+            var expectedWord = "t35t";
+            _mockWordApplication.Setup(res => res.AddWord(expectedWord)).ReturnsAsync(false);
+
+            // Act            
+            var wordController = new WordLabController(_mockWordApplication.Object);
+            var result = await wordController.InsertionWord(expectedWord);
+
+            // Assert
+            Assert.IsType<StatusCodeResult>(result);
+            var statusCodeResult = (StatusCodeResult)result;
+            Assert.Equal(400, statusCodeResult.StatusCode);
         }
     }
 }
