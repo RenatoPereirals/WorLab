20d596a0a1f8139c81ddfd062a6edeae4e475860
RenatoPereirals <renatopreirals@gmail.com>
2024-05-15 22:48:56 -0300

    test: Refactor and add additional tests in WordLabControllerTest
    - Refactor existing tests and add new tests to improve test coverage
    - Add test InsertionWord_ReturnsBadRequest_WhenWordInsertedIsNullOrEmpty
    - Add test InsertionWord_ReturnsInternalServerError_WhenExceptionOccurs
    
    #5

--- test/unit.tests/API/WordLabControllerTest.cs
+++ test/unit.tests/API/WordLabControllerTest.cs
@@ -1,24 +1,22 @@
 using Moq;
-
 using Microsoft.AspNetCore.Mvc;
-
 using WordLab.API.Controllers;
 using WordLab.API.Interfaces;
-using Xunit.Sdk;
+using Xunit;
 
 namespace test.unit.tests.API
 {
-    public class WordLabCotrollerTest
+    public class WordLabControllerTest
     {
         private readonly Mock<IWordApplication> _mockWordApplication;
 
-        public WordLabCotrollerTest()
+        public WordLabControllerTest()
         {
             _mockWordApplication = new Mock<IWordApplication>();
         }
 
         [Fact]
-        public async void InsertWord_WhenWordInserted_ReturnsSuccessWithStatusCode201()
+        public async Task InsertionWord_ReturnsSuccessWithStatusCode201_WhenWordInserted()
         {
             // Arrange
             var expectedWord = "test";
@@ -30,12 +28,32 @@ namespace test.unit.tests.API
 
             // Assert
             Assert.IsType<StatusCodeResult>(result);
-            var statusCodeResult = (StatusCodeResult)result;
-            Assert.Equal(201, statusCodeResult.StatusCode);
+            Assert.Equal(201, (result as StatusCodeResult)?.StatusCode);
         }
 
         [Fact]
-        public async void InsertWord_WhenWordInserted_ReturnsExceptionWithStatusCode400()
+        public async Task InsertionWord_ReturnsBadRequest_WhenWordInsertedIsNullOrEmpty()
+        {
+            // Arrange
+            var wordController = new WordLabController(_mockWordApplication.Object);
+            string? nullWord = null;
+            string emptyWord = "";
+
+            // Act
+            var resultForNullWord = await wordController.InsertionWord(nullWord!) as BadRequestObjectResult;
+            var resultForEmptyWord = await wordController.InsertionWord(emptyWord) as BadRequestObjectResult;
+
+            // Assert
+            Assert.NotNull(resultForNullWord);
+            Assert.NotNull(resultForEmptyWord);
+            Assert.Equal(400, resultForNullWord.StatusCode);
+            Assert.Equal(400, resultForEmptyWord.StatusCode);
+            Assert.Equal("A palavra não pode ser nula ou vazia.", resultForNullWord.Value);
+            Assert.Equal("A palavra não pode ser nula ou vazia.", resultForEmptyWord.Value);
+        }
+
+        [Fact]
+        public async Task InsertionWord_ReturnsBadRequest_WhenWordNotInserted()
         {
             // Arrange
             var expectedWord = "t35t";
@@ -47,12 +65,11 @@ namespace test.unit.tests.API
 
             // Assert
             Assert.IsType<StatusCodeResult>(result);
-            var statusCodeResult = (StatusCodeResult)result;
-            Assert.Equal(400, statusCodeResult.StatusCode);
+            Assert.Equal(400, (result as StatusCodeResult)?.StatusCode);
         }
 
         [Fact]
-        public async void InsertWord_WhenWordInserted_ReturnsExceptionWithStatusCode500()
+        public async Task InsertionWord_ReturnsInternalServerError_WhenExceptionOccurs()
         {
             // Arrange
             var expectedWord = "test";
@@ -64,8 +81,7 @@ namespace test.unit.tests.API
 
             // Assert
             Assert.IsType<StatusCodeResult>(result);
-            var statusCodeResult = (StatusCodeResult)result;
-            Assert.Equal(500, statusCodeResult.StatusCode);
+            Assert.Equal(500, (result as StatusCodeResult)?.StatusCode);
         }
     }
 }
