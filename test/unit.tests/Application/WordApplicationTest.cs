6cfd7d731babe031b461d22632941f631d125dc2
RenatoPereirals <renatopreirals@gmail.com>
2024-05-16 23:23:28 -0300

    test: Refactor WordApplicationTest with mock setup methods
    - Refactor WordApplicationTest to include setup methods for valid and invalid words
    - Utilize mock setup methods to centralize common test setup logic
    
    #3

--- test/unit.tests/Application/WordApplicationTest.cs
+++ test/unit.tests/Application/WordApplicationTest.cs
@@ -9,62 +9,60 @@ namespace test.unit.tests.Application
         private readonly Mock<ISpellCheckService> _mockSpellCheck;
         private readonly Mock<IWordValidator> _mockWordValidator;
         private readonly Mock<IWordRepository> _mockWordRepository;
+        private readonly WordApplication _application;
 
         public WordApplicationTest()
         {
             _mockSpellCheck = new Mock<ISpellCheckService>();
             _mockWordValidator = new Mock<IWordValidator>();
             _mockWordRepository = new Mock<IWordRepository>();
+
+            _application = new WordApplication(
+                _mockSpellCheck.Object,
+                _mockWordValidator.Object,
+                _mockWordRepository.Object
+            );
         }
 
         [Fact]
-        public async Task AddWordAsync_ShouldReturnTrue_WhenWordIsAdded()
+        public async Task AddWordAsync_ReturnsTrue_WhenWordIsValidAndAddedSuccessfully()
         {
-            // Arrage
-            var wordToAdd = "test";
-            var application = new WordApplication(_mockSpellCheck.Object, _mockWordValidator.Object, _mockWordRepository.Object);
-            _mockSpellCheck.Setup(res => res.VerifySpellingAsync(wordToAdd)).ReturnsAsync(true);
-            _mockWordValidator.Setup(res => res.IsValidWord(wordToAdd)).ReturnsAsync(true);
-            _mockWordRepository.Setup(res => res.AddAsync(wordToAdd)).ReturnsAsync(true);
+            // Arrange
+            var validWord = "test";
+            SetupMocksForValidWord(validWord);
 
             // Act
-            var result = await application.AddWordAsync(wordToAdd);
+            var result = await _application.AddWordAsync(validWord);
 
             // Assert
             Assert.True(result);
         }
 
         [Fact]
-        public async Task AddWordAsync_ShouldReturnTrue_WhenWordIsValid()
+        public async Task AddWordAsync_ReturnsFalse_WhenWordIsInvalid()
         {
-            // Arrage
-            var wordToAdd = "t35t";
-            var application = new WordApplication(_mockSpellCheck.Object, _mockWordValidator.Object, _mockWordRepository.Object);
-            _mockSpellCheck.Setup(res => res.VerifySpellingAsync(wordToAdd)).ReturnsAsync(true);
-            _mockWordValidator.Setup(res => res.IsValidWord(wordToAdd)).ReturnsAsync(true);
-            _mockWordRepository.Setup(res => res.AddAsync(wordToAdd)).ReturnsAsync(true);
+            // Arrange
+            var invalidWord = "t35t";
+            SetupMocksForInvalidWord(invalidWord);
 
             // Act
-            var result = await application.AddWordAsync(wordToAdd);
-
-            // Assert
-            Assert.True(result);
-        }
-
-        [Fact]
-        public async Task AddWordAsync_ShouldReturnFalse_WhenWordIsInvalid()
-        {
-            // Arrage
-            var wordToAdd = "t35t";
-            var application = new WordApplication(_mockSpellCheck.Object, _mockWordValidator.Object, _mockWordRepository.Object);
-            _mockSpellCheck.Setup(res => res.VerifySpellingAsync(wordToAdd)).ReturnsAsync(false);
-            _mockWordValidator.Setup(res => res.IsValidWord(wordToAdd)).ReturnsAsync(false);
-
-            // Act
-            var result = await application.AddWordAsync(wordToAdd);
+            var result = await _application.AddWordAsync(invalidWord);
 
             // Assert
             Assert.False(result);
+        }
+
+        private void SetupMocksForValidWord(string word)
+        {
+            _mockSpellCheck.Setup(service => service.VerifySpellingAsync(word)).ReturnsAsync(true);
+            _mockWordValidator.Setup(validator => validator.IsValidWord(word)).ReturnsAsync(true);
+            _mockWordRepository.Setup(repo => repo.AddAsync(word)).ReturnsAsync(true);
+        }
+
+        private void SetupMocksForInvalidWord(string word)
+        {
+            _mockSpellCheck.Setup(service => service.VerifySpellingAsync(word)).ReturnsAsync(false);
+            _mockWordValidator.Setup(validator => validator.IsValidWord(word)).ReturnsAsync(false);
         }
     }
 }
