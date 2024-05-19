e8616e0a8dfa185ef39f32c6e676d3342d514165
RenatoPereirals <renatopreirals@gmail.com>
2024-05-19 10:59:16 -0300

    feat: Add word existence check in AddWordAsync method
    - Updated AddWordAsync method to check if a word already exists before adding.
    - Added WordExists method to check the existence of a word in the persistence layer.
    - Included a new condition in AddWordAsync to return false if the word already exists.
    
    #4

--- src/WordLab.Application/Services/WordApplication.cs
+++ src/WordLab.Application/Services/WordApplication.cs
@@ -5,25 +5,33 @@ namespace WordLab.Application.Services
     public class WordApplication(ISpellCheckService spellCheck,
                                  IWordValidator wordValidator,
                                  IWordRepository wordRepository,
-                                 IWordService wordService)
+                                 IWordService wordService,
+                                 IWordPersistence wordPersistence)
     {
         private readonly ISpellCheckService _spellCheck = spellCheck ??
             throw new ArgumentNullException(nameof(spellCheck));
+
         private readonly IWordValidator _wordValidator = wordValidator ??
             throw new ArgumentNullException(nameof(wordValidator));
+
         private readonly IWordRepository _wordRepository = wordRepository ??
             throw new ArgumentNullException(nameof(wordRepository));
+
         private readonly IWordService _wordService = wordService ??
             throw new ArgumentNullException(nameof(wordService));
+
+        private readonly IWordPersistence _wordPersistence = wordPersistence ??
+            throw new ArgumentNullException(nameof(wordPersistence));
 
         public async Task<bool> AddWordAsync(string word)
         {
             if (string.IsNullOrWhiteSpace(word))
-            {
                 throw new ArgumentException("A palavra não pode ser nula ou conter espaços vazios.", nameof(word));
-            }
 
             if (!await _spellCheck.VerifySpellingAsync(word) && !await _wordValidator.IsValidWord(word))
+                return false;
+
+            if (await WordExists(word))
                 return false;
 
             try
@@ -33,9 +41,14 @@ namespace WordLab.Application.Services
             }
             catch (Exception ex)
             {
-                // Log the exception (logging not shown here)
+                // Log the exception
                 throw new ApplicationException("Erro ao classificar a palavra.", ex);
             }
+        }
+
+        public async Task<bool> WordExists(string word)
+        {
+            return await _wordPersistence.GetWordByWord(word);
         }
     }
 }
