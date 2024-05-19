641efc747253e2a3fde1c5b1b148767ac63d607a
RenatoPereirals <renatopreirals@gmail.com>
2024-05-19 11:02:33 -0300

    feat: Add WordExists contract to IWordApplication interface
    - Added the contract Task<bool> WordExists(string word) to the IWordApplication interface.
    
    #2

--- src/WordLab.Application/Interfaces/IWordApplication.cs
+++ src/WordLab.Application/Interfaces/IWordApplication.cs
@@ -1,7 +1,8 @@
 namespace WordLab.Application.Interfaces
 {
-    public interface IWordApplication 
+    public interface IWordApplication
     {
         Task<bool> AddWord(string word);
-     }
+        Task<bool> WordExists(string word);
+    }
 }
