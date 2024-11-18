568227a262faff534f487cc9806126dae73538db
renatoPreirals <renatopreirals@gmail.com>
2024-11-18 16:55:35 -0300

    refactor: Update WordApplication service to improve error handling and logging
    - Added custom `WordApplicationException` to improve exception specificity.
    - Refined error messages for null or empty word validations.
    - Updated `WordExists` method to handle null checks on retrieved word entity.
    - Enhanced logging for error scenarios in `AddWordAsync` and `WordExists` methods.
    - Improved exception handling and user feedback in methods where word validation and classification are performed.

--- src/WordLab.Application/Services/WordApplication.cs
+++ src/WordLab.Application/Services/WordApplication.cs
@@ -1,67 +1,64 @@
 using WordLab.Domain.Entity;
 using WordLab.Domain.Interfaces;
 
-namespace WordLab.Application.Services
+using WordLab.Application.Exceptions;
+
+namespace WordLab.Application.Services;
+
+public class WordApplication(ISpellCheckService spellCheck,
+                             IWordValidator wordValidator,
+                             IWordRepository wordRepository,
+                             IWordService wordService)
 {
-    public class WordApplication(ISpellCheckService spellCheck,
-                                 IWordValidator wordValidator,
-                                 IWordRepository wordRepository,
-                                 IWordService wordService)
+    private readonly ISpellCheckService _spellCheck = spellCheck ?? throw new ArgumentNullException(nameof(spellCheck));
+
+    private readonly IWordValidator _wordValidator = wordValidator ?? throw new ArgumentNullException(nameof(wordValidator));
+
+    private readonly IWordRepository _wordRepository = wordRepository ?? throw new ArgumentNullException(nameof(wordRepository));
+
+    private readonly IWordService _wordService = wordService ?? throw new ArgumentNullException(nameof(wordService));
+
+    public async Task<bool> AddWordAsync(string word)
     {
-        private readonly ISpellCheckService _spellCheck = spellCheck ??
-            throw new ArgumentNullException(nameof(spellCheck));
+        if (string.IsNullOrWhiteSpace(word))
+            throw new ArgumentException("The word cannot be null or contain empty spaces.", nameof(word));
 
-        private readonly IWordValidator _wordValidator = wordValidator ??
-            throw new ArgumentNullException(nameof(wordValidator));
+        if (!await _spellCheck.VerifySpellingAsync(word) && !await _wordValidator.IsValidWord(word))
+            return false;
 
-        private readonly IWordRepository _wordRepository = wordRepository ??
-            throw new ArgumentNullException(nameof(wordRepository));
-
-        private readonly IWordService _wordService = wordService ??
-            throw new ArgumentNullException(nameof(wordService));
-
-        public async Task<bool> AddWordAsync(string word)
+        try
         {
-            if (string.IsNullOrWhiteSpace(word))
-                throw new ArgumentException("A palavra não pode ser nula ou conter espaços vazios.", nameof(word));
-
-            if (!await _spellCheck.VerifySpellingAsync(word) && !await _wordValidator.IsValidWord(word))
+            if (await WordExists(word))
                 return false;
 
-            try
-            {
-                if (await WordExists(word))
-                    return false;
-
-                ClassifiedWord classifiedWord = await _wordService.ClassifyWordAsync(word);
-                return await _wordRepository.AddAsync(classifiedWord);
-            }
-            catch (Exception ex)
-            {
-                // Log the exception
-                throw new ApplicationException("Erro ao classificar a palavra.", ex);
-            }
+            ClassifiedWord classifiedWord = await _wordService.ClassifyWordAsync(word);
+            return await _wordRepository.AddAsync(classifiedWord);
         }
-
-        public async Task<bool> WordExists(string word)
+        catch (Exception ex)
         {
-            try
-            {
-                if (string.IsNullOrEmpty(word))
-                    throw new ArgumentNullException(nameof(word), "Palavra não pode ser nula ou vazia.");
+            Console.WriteLine($"Error adding the word: {ex.Message}");
+            throw new ApplicationException("Error classifying the word.", ex);
+        }
+    }
 
-                Word wordName = await _wordRepository.GetWordByWord(word);
+    public async Task<bool> WordExists(string word)
+    {
+        try
+        {
+            if (string.IsNullOrEmpty(word))
+                throw new ArgumentNullException(nameof(word), "The word cannot be null or contain empty spaces.");
 
-                if (wordName.Name.Equals(word, StringComparison.CurrentCultureIgnoreCase))
-                    return true;
-                else
-                    return false;
-            }
-            catch (Exception)
-            {
-                // Log de error
-                throw;
-            }
+            Word wordName = await _wordRepository.GetWordByWord(word);
+
+            if (wordName != null && wordName.Name.Equals(word, StringComparison.CurrentCultureIgnoreCase))
+                return true;
+
+            return false;
+        }
+        catch (WordApplicationException ex)
+        {
+            Console.WriteLine($"Error checking if the word exists: {ex.Message}");
+            throw new WordApplicationException("An internal error occurred, please try again later.");
         }
     }
 }
